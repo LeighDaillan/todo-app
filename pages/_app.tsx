f82f5b0607e2879d8506891076207d6e25f8d64f
DaillanLeigh <francodaillanleigh@gmail.com>
2023-01-31 19:20:09 +0800

    Added session provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,22 @@
-import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layouts/layout";
 import { Poppins } from "@next/font/google";
+import "../styles/globals.css";
 
 const poppins = Poppins({ weight: ["400"], subsets: ["latin"] });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <main className={poppins.className}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </main>
+    <SessionProvider session={session}>
+      <main className={poppins.className}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </main>
+    </SessionProvider>
   );
 }
